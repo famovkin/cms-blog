e3855f3ed28bff8c2bf60fdb23d57c842468d4d5
Tolya Fam <famovkin@yandex.ru>
2022-06-01 22:42:57 +0300

    style: format code

--- contentful/index.ts
+++ contentful/index.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful';
 
 const client = createClient({
-    space: 'x6qlslf70m1n',
-    accessToken: 'wyzNYl5xFg0Fdh2o5N8vBeyz9rhFRW2ciJ99SjiKiu4',
+  space: 'x6qlslf70m1n',
+  accessToken: 'wyzNYl5xFg0Fdh2o5N8vBeyz9rhFRW2ciJ99SjiKiu4',
 });
 
 export default client;
